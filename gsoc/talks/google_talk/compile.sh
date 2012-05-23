ab8b5370bb028d97befd8fe6a26128f418e11b01
Guru Devanla <grdvnl@gmail.com>
2012-05-22 22:18:01 -0500

    initial version

---
+++ gsoc/talks/google_talk/compile.sh
@@ -0,0 +1,13 @@
+#dvips fig.dvi
+#ps2pdf fig.ps
+
+#latex ModAlg.tex
+#bibtex ModAlg.aux
+#latex ModAlg.tex
+#latex ModAlg.tex
+
+latex gsoc_gtalk.tex
+dvips gsoc_gtalk.dvi 
+ps2pdf gsoc_gtalk.ps 
+
+
