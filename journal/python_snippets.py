c8986247b5eca3db54471b7c3a648d0977370ae3
Guru Devanla <grdvnl@gmail.com>
2012-11-11 12:08:03 -0600

    general backup

---
+++ journal/python_snippets.py
@@ -0,0 +1,30 @@
+#~/usr/bin/python
+
+
+
+1. splitting strings based on separators
+2. tansposing neighor chars of string
+3. slicing strings
+
+list
+
+1. sort list
+2. sort list with tuples in them
+3. sort using lambda functions
+4. sort using comparators
+5. reverse operations
+
+3. create dicts with defailt value datatype
+4. get key amd value for dicts at same time
+5. emumerate()/itertools.
+
+
+4. open, read files
+5. list comprehension multiple for
+
+
+
+python built-in types page
+python functions page
+python sets
+
